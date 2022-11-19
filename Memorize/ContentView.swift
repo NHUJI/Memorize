0b31eb47bf687cce2b43ed92bab0b87161d23609
NHUJI <cnzhouchang@gmail.com>
2022-11-19 11:31:35 +0800

    扩展了一行卡片并且可以点击还有依靠数组快速修改内容了 Expanded to a line of cards and can now click and rely on an array to quickly modify the content

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -10,22 +10,59 @@ import SwiftUI
 
 //结构:变量的集合 不仅有变量也可以有函数
 struct ContentView: View {
+    var emojis = ["🍙", "🍰", "🧁", "🍭"]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-                
+        HStack{
+            CardView(Content: emojis[0])
+            CardView(Content: emojis[1])
+            CardView(Content: emojis[2])
+            CardView(Content: emojis[3])
         }
-        .padding()
+        .padding(.vertical)
+        .foregroundColor(Color.red)
     }
 }
+
+struct CardView : View{
+    var Content:String
+    @State var isFaceUp : Bool = true
+    
+    var body: some View{
+        ZStack{
+            let shape = RoundedRectangle(cornerRadius: 20)
+            if isFaceUp{
+                shape.fill().foregroundColor(.white)
+                shape.stroke(lineWidth: 3)
+                Text(Content).font(.largeTitle)
+            }else{
+                shape.fill()
+            }
+        }
+        .onTapGesture {
+            isFaceUp = !isFaceUp
+        }
+    }
+}
+   
+
+
+
+
+
+
+
+
+
+
 
 
 //preview需要的,不需要看它
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
+        //可以通过设置多个ContentView()来看不同的模拟效果
         ContentView()
+        ContentView()
+            .preferredColorScheme(.dark)
     }
 }
